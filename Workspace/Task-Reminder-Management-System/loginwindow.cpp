60b6f9b308399fcbdd0574d4f60414e4b9d6c51c
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2020-12-26 14:16:51 +0530

    Initial Qt Project
    Newly created Qt Widgets Application project.

---
+++ Workspace/Task-Reminder-Management-System/loginwindow.cpp
@@ -0,0 +1,21 @@
+/***********************************************
+**
+**
+**
+***********************************************/
+
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+
+LoginWindow::LoginWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LoginWindow)
+{
+    ui->setupUi(this);
+}
+
+LoginWindow::~LoginWindow()
+{
+    delete ui;
+}
+
---
+++ Workspace/build-Task-Reminder-Management-System-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_loginwindow.h
@@ -0,0 +1,61 @@
+/********************************************************************************
+** Form generated from reading UI file 'loginwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LOGINWINDOW_H
+#define UI_LOGINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_LoginWindow
+{
+public:
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *LoginWindow)
+    {
+        if (LoginWindow->objectName().isEmpty())
+            LoginWindow->setObjectName(QString::fromUtf8("LoginWindow"));
+        LoginWindow->resize(800, 600);
+        centralwidget = new QWidget(LoginWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        LoginWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(LoginWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        LoginWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(LoginWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        LoginWindow->setStatusBar(statusbar);
+
+        retranslateUi(LoginWindow);
+
+        QMetaObject::connectSlotsByName(LoginWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *LoginWindow)
+    {
+        LoginWindow->setWindowTitle(QCoreApplication::translate("LoginWindow", "LoginWindow", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class LoginWindow: public Ui_LoginWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LOGINWINDOW_H
